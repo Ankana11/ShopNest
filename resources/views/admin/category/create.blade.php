2d7dd10c6da457c1462e2548f57c06356fb1eb6c
Ankana <reach2ankana@gmail.com>
2024-08-12 21:02:13 +0530

    category

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -9,30 +9,27 @@
             <div class="col-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">Basic form elements</h4>
-                  <p class="card-description"> Basic form elements </p>
-                  <form class="forms-sample">
+                  <h4 class="card-title">Create Categoris</h4>
+                
+                  <form class="forms-sample" action="" method="POST" name="categoryForm" id="categoryForm">
                     <div class="form-group">
                       <label for="exampleInputName1">Name</label>
                       <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
                     </div>
+                    
                     <div class="form-group">
-                      <label for="exampleInputEmail3">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+                      <label for="exampleInputPassword4">Slug</label>
+                      <input type="text" class="form-control" id="exampleInputPassword4" placeholder="Slug">
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputPassword4">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleSelectGender">Gender</label>
+                      <label for="exampleSelectGender">Status</label>
                       <select class="form-control" id="exampleSelectGender">
-                        <option>Male</option>
-                        <option>Female</option>
+                        <option>Active</option>
+                        <option>InActive</option>
                       </select>
                     </div>
                     <div class="form-group">
-                      <label>File upload</label>
+                      <label>Image upload</label>
                       <input type="file" name="img[]" class="file-upload-default">
                       <div class="input-group col-xs-12">
                         <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
@@ -41,14 +38,8 @@
                         </span>
                       </div>
                     </div>
-                    <div class="form-group">
-                      <label for="exampleInputCity1">City</label>
-                      <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleTextarea1">Textarea</label>
-                      <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-                    </div>
+                    
+                  
                     <button type="submit" class="btn btn-primary mr-2">Submit</button>
                     <button class="btn btn-dark">Cancel</button>
                   </form>
